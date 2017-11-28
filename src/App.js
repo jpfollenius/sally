2667ccc3b506136564b445b6e2484812d6e3e1fb
jpf <jpfollenius@gmail.com>
2017-11-28 14:52:03 +0100

    Added firebase

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import firebase from 'firebase';
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAyU2GCASUk-JDXW4DAeRsEPIK2105-JwA",
+  authDomain: "sally-backend.firebaseapp.com",
+  databaseURL: "https://sally-backend.firebaseio.com",
+  projectId: "sally-backend",
+  storageBucket: "sally-backend.appspot.com",
+  messagingSenderId: "1048787517635"
+};
+firebase.initializeApp(config);
 
 class App extends Component {
   render() {    
